d1a90d49ad5072bcee03cfa3c852c0a5268ea268
June <her231038@uvg.edu.gt>
2023-11-06 09:07:29 -0600

    Creación de proyecto JAVAFx
    Creación de proyecto generado en IntellijIDEA.

---
+++ src/main/java/com/example/uvgmeetdemo/Main.java
@@ -0,0 +1,19 @@
+package com.example.uvgmeetdemo;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+    private Stage primaryStage;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+
+    }
+}
